39a276d8e3a107cfc925ef83de3ccd38395920c0
Vangmay <78148958+Vangmay@users.noreply.github.com>
2021-04-19 15:22:18 +0530

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,6 +1,6 @@
 msg_index = []
 Alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-shift = int(input("Enter the number of how much shift"))
+shift = int(input("Enter the number for shift-size"))
 Cipher = Alphabet[shift:] + Alphabet[:shift]
 message = str(input("What message do you want to decode "))
 def split(message):
